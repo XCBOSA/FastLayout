1203d53340143172090716256047c2a032cebacc
xcbosa <xcbosa@forgetive.org>
2021-12-04 23:01:53 +0800

    Add @discardableResult to extension.

--- Sources/FastLayout/FastLayout.swift
+++ Sources/FastLayout/FastLayout.swift
@@ -1,78 +1,96 @@
 import UIKit
 
 extension NSLayoutDimension {
+    @discardableResult
     public static func == (lhs: NSLayoutDimension, rhs: NSLayoutDimension) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(equalTo: $1) }
     }
     
+    @discardableResult
     public static func == (lhs: NSLayoutDimension, rhs: FLAnchorWithOffset<NSLayoutDimension>) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(equalTo: $1.baseAnchor, constant: $1.offset) }
     }
     
+    @discardableResult
     public static func <= (lhs: NSLayoutDimension, rhs: NSLayoutDimension) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(lessThanOrEqualTo: $1) }
     }
     
+    @discardableResult
     public static func <= (lhs: NSLayoutDimension, rhs: FLAnchorWithOffset<NSLayoutDimension>) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(lessThanOrEqualTo: $1.baseAnchor, constant: $1.offset) }
     }
     
+    @discardableResult
     public static func >= (lhs: NSLayoutDimension, rhs: NSLayoutDimension) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(greaterThanOrEqualTo: $1) }
     }
     
+    @discardableResult
     public static func >= (lhs: NSLayoutDimension, rhs: FLAnchorWithOffset<NSLayoutDimension>) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(greaterThanOrEqualTo: $1.baseAnchor, constant: $1.offset) }
     }
 }
 
 extension NSLayoutXAxisAnchor {
+    @discardableResult
     public static func == (lhs: NSLayoutXAxisAnchor, rhs: NSLayoutXAxisAnchor) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(equalTo: $1) }
     }
     
+    @discardableResult
     public static func == (lhs: NSLayoutXAxisAnchor, rhs: FLAnchorWithOffset<NSLayoutXAxisAnchor>) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(equalTo: $1.baseAnchor, constant: $1.offset) }
     }
     
+    @discardableResult
     public static func <= (lhs: NSLayoutXAxisAnchor, rhs: NSLayoutXAxisAnchor) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(lessThanOrEqualTo: $1) }
     }
     
+    @discardableResult
     public static func <= (lhs: NSLayoutXAxisAnchor, rhs: FLAnchorWithOffset<NSLayoutXAxisAnchor>) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(lessThanOrEqualTo: $1.baseAnchor, constant: $1.offset) }
     }
     
+    @discardableResult
     public static func >= (lhs: NSLayoutXAxisAnchor, rhs: NSLayoutXAxisAnchor) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(greaterThanOrEqualTo: $1) }
     }
     
+    @discardableResult
     public static func >= (lhs: NSLayoutXAxisAnchor, rhs: FLAnchorWithOffset<NSLayoutXAxisAnchor>) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(greaterThanOrEqualTo: $1.baseAnchor, constant: $1.offset) }
     }
 }
 
 extension NSLayoutYAxisAnchor {
+    @discardableResult
     public static func == (lhs: NSLayoutYAxisAnchor, rhs: NSLayoutYAxisAnchor) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(equalTo: $1) }
     }
     
+    @discardableResult
     public static func == (lhs: NSLayoutYAxisAnchor, rhs: FLAnchorWithOffset<NSLayoutYAxisAnchor>) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(equalTo: $1.baseAnchor, constant: $1.offset) }
     }
     
+    @discardableResult
     public static func <= (lhs: NSLayoutYAxisAnchor, rhs: NSLayoutYAxisAnchor) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(lessThanOrEqualTo: $1) }
     }
     
+    @discardableResult
     public static func <= (lhs: NSLayoutYAxisAnchor, rhs: FLAnchorWithOffset<NSLayoutYAxisAnchor>) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(lessThanOrEqualTo: $1.baseAnchor, constant: $1.offset) }
     }
     
+    @discardableResult
     public static func >= (lhs: NSLayoutYAxisAnchor, rhs: NSLayoutYAxisAnchor) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(greaterThanOrEqualTo: $1) }
     }
     
+    @discardableResult
     public static func >= (lhs: NSLayoutYAxisAnchor, rhs: FLAnchorWithOffset<NSLayoutYAxisAnchor>) -> NSLayoutConstraint {
         FLCreateAndActiveConstraint(lhs, rhs) { $0.constraint(greaterThanOrEqualTo: $1.baseAnchor, constant: $1.offset) }
     }
