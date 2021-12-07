ca7507584984938cef5a600e1199d3d5038d98e0
xcbosa <xcbosa@forgetive.org>
2021-12-08 00:55:00 +0800

    ADD: Improve FLViewState API.

--- Sources/FastLayout/FLViewState.swift
+++ Sources/FastLayout/FLViewState.swift
@@ -9,12 +9,22 @@ import UIKit
 
 public typealias FLViewStateCheckerBlock = (CGFloat, CGFloat) -> Bool
 
+public let FLViewStateCheckerBlockHorizontal: FLViewStateCheckerBlock = { $0 > $1 }
+public let FLViewStateCheckerBlockPortrait: FLViewStateCheckerBlock = { $0 < $1 }
+
 public class FLViewState {
     public private(set) var constraintGroup = [NSLayoutConstraint]()
     public private(set) var checker: FLViewStateCheckerBlock
+    public private(set) var recorder: FLConstraintRecorder?
     
     public init (checkerBlock: @escaping FLViewStateCheckerBlock) {
         self.checker = checkerBlock
+    }
+    
+    public init (configureWithRecorder recorder: FLConstraintRecorder, checkerBlock: @escaping FLViewStateCheckerBlock) {
+        self.checker = checkerBlock
+        self.recorder = recorder
+        self.recorder!.beginRecord()
     }
     
     public func append(_ constraintObject: NSLayoutConstraint) {
@@ -24,6 +34,14 @@ public class FLViewState {
     
     public func append(contentsOf contents: [NSLayoutConstraint]) {
         _ = contents.map({ self.append($0) })
+    }
+    
+    public func finishRecorder() {
+        guard let recorder = self.recorder else {
+            fatalError("FLViewState.finishRecorder() must called on object which init with init(configureWithRecorder: checkerBlock:), or FLViewState.finishRecorder() has already called on this object.")
+        }
+        self.append(contentsOf: recorder.endRecord())
+        self.recorder = nil
     }
     
     public var isActive: Bool = false {
